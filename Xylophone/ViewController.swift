cc8a9106639e092d2831bd935dafe44125d632a6
Riandro Proenca <riandro95@gmail.com>
2021-09-06 18:09:28 -0300

    add functions and mp3 player

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
+    @IBAction func keyPressedC(_ sender: UIButton) {
+        
+        playSound(som:sender.currentTitle!)
+        
+    }
+    func playSound(som:String) {
+        let seconds = 0.4
+        DispatchQueue.main.asyncAfter(deadline:.now() + seconds){ [self] in
+            let url = Bundle.main.url(forResource: som, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            
+            player.play()
+        }
+        
 
+    }
+    
 }
 
